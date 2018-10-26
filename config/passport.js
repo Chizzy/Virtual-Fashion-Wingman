0527b932ab9a58446ec9ce0d3af7cd3bcb30c909
Chizzy <chizoba.princess@gmail.com>
2018-10-26 14:43:25 -0400

    added logic to signup user that needs help

---
+++ config/passport.js
@@ -0,0 +1,33 @@
+const LocalStrategy = require('passport-local').Strategy
+const UserHelp = require('../models/UserHelp')
+const UserStylist = require('../models/UserStylist')
+
+const passportConfig = (passport) => {
+    const strategyFields = {
+        emailField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }
+
+    const localSignupCallback = (req, email, password, callback) => {
+        UserHelp.findOne({'local.email': email}).then((userHelp) => {
+            if (userHelp) {
+                return callback (null, false, req.flash('signupMessage', 'This email is already in use.'))
+            } else {
+                const newUserHelp = new UserHelp()
+                newUserHelp.local.email = email
+                newUserHelp.local.password = newUserHelp.encrypt(password)
+                newUserHelp.save().then((saved) => {
+                    return callback (null, saved)
+                })
+            }
+        })
+
+
+    
+    }
+
+    passport.use('local-signup', new LocalStrategy(strategyFields, localSignupCallback))
+}
+
+module.exports = passportConfig
