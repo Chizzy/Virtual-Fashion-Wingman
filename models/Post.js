d6b894df6a767fa3f868dde0e2c3d7a63a79f2cd
Chizzy <chizoba.princess@gmail.com>
2018-10-26 11:58:30 -0400

    created the post schema

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,16 @@ const mongoose = require('../db/connection')
 const Schema = mongoose.Schema
 
 const Post = new Schema ({
-    
+    img: String,
+    creator: {
+        type: Schema.Types.UserStylist,
+        ref: 'UserHelp'
+    }, 
+    outfit: String,
+    date: {
+        type: Date,
+        default: Date.now()
+    }
 })
 
 module.exports = mongoose.model('Post', Post)
