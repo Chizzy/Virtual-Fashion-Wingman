6517045ad5f9529140effe9f325909f8d498a757
Chizzy <chizoba.princess@gmail.com>
2018-10-29 11:07:20 -0400

    created a POST create route for userStylist

--- controllers/userStylist.js
+++ controllers/userStylist.js
@@ -7,6 +7,9 @@ const userStylistController = {
     },
     show: (req, res) => {
         res.send('Waz hannin from UserStylist show!')
+    },
+    create: (req, res) => {
+        res.send('Waz hannin from UserStylist create!')
     }
 }
 
