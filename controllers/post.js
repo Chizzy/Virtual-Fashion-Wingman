32127c2e37fc2ea9adf9126eaa3c7b8d3f8d91a4
Chizzy <chizoba.princess@gmail.com>
2018-10-31 00:15:18 -0400

    created views page for new post

--- controllers/post.js
+++ controllers/post.js
@@ -4,17 +4,25 @@ const Post = require('../models/Post')
 const postController = {
     index: (req, res) => {
         Post.find().populate('creator').then((posts) => {
-            res.render('post/index', {posts: posts})
+            res.render('post/index', {
+                posts: posts
+            })
         })
     },
     new: (req, res) => {
-        res.send('Waz hannin from Post new!')
+        res.render('post/new')
     },
     show: (req, res) => {
         res.send('Waz hannin from Post show!')
     },
     create: (req, res) => {
-        res.send('Waz hannin from Post create!')
+        Post.create({
+            img: req.body.img,
+            creator: req.user._id,
+            content: req.body.content
+        }).then((newPost) => {
+            res.redirect(`/posts/${newPost._id}`)
+        })
     },
     edit: (req, res) => {
         res.send('Waz hannin from Post edit!')
